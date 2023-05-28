7dc16b32ff6c86ab1d53903b4f1b315f9662216c
imretab <imre.tabajdi@gmail.com>
2023-05-28 14:44:13 +0200

    Logger teszt - Többi a leírásban
    - Logger jó, plusz néhány legenerált xml fájl

--- ProgTech_Beadando/PC_Builder/src/Main.java
+++ ProgTech_Beadando/PC_Builder/src/Main.java
@@ -1,5 +1,8 @@
+import org.apache.log4j.Logger;
 public class Main {
+    private static Logger log = Logger.getLogger(Main.class);
     public static void main(String[] args) {
-
+        log.info("Hello there");
+        System.out.println("Logging works");
     }
 }
