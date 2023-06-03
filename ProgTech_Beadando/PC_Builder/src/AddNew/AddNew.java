a584e930632bcf52254ad5a5c50f5d7a0d2fcf53
imretab <imre.tabajdi@gmail.com>
2023-06-03 17:35:12 +0200

    Kisebb javítások - Imretab

--- ProgTech_Beadando/PC_Builder/src/AddNew/AddNew.java
+++ ProgTech_Beadando/PC_Builder/src/AddNew/AddNew.java
@@ -1,8 +1,12 @@
 package AddNew;
 
 import DBconnection.DBconnection;
+import DBconnection.GetAllComponentsCommand;
 
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.sql.ResultSet;
 import java.util.logging.Logger;
 
 public class AddNew {
@@ -15,12 +19,15 @@ public class AddNew {
     private JComboBox cb_tap;
     private JComboBox cb_gephaz;
     private JPanel BuildPanel;
+    private JButton btn_beszur;
     private JFrame frame;
     private DBconnection dBconnection;
 
     public AddNew(DBconnection dBconnection) {
         this.dBconnection = dBconnection;
+        GetAll();
         ConfigureFrame();
+        RegisterListeners();
     }
     private void ConfigureFrame(){
         this.frame = new JFrame();
@@ -30,5 +37,27 @@ public class AddNew {
         frame.pack();
         frame.setVisible(true);
         logger.info("Show Build frame configured");
+    }
+    private void RegisterListeners(){
+        btn_beszur.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                //Insert();
+            }
+        });
+    }
+    private void GetAll(){
+        GetAllComponentsCommand getAllComponentsCommand = new GetAllComponentsCommand(this.dBconnection);
+        getAllComponentsCommand.execute();
+        try{
+            ResultSet mobo = getAllComponentsCommand.getMobo();
+            logger.info(mobo.getString("gyarto"));
+            while (mobo.next()){
+                cb_alaplap.addItem(mobo.getString("gyarto"));
+            }
+        }
+        catch (Exception e){
+            logger.warning(e.getMessage());
+        }
     }
 }
--- ProgTech_Beadando/PC_Builder/src/DBconnection/GetAllComponentsCommand.java
+++ ProgTech_Beadando/PC_Builder/src/DBconnection/GetAllComponentsCommand.java
@@ -4,39 +4,122 @@ import Objects.PC_object;
 
 import javax.swing.*;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class GetAllComponentsCommand implements Command{
     private static Logger logger = Logger.getLogger("Getting all the components");
     private PC_object newPC;
-    private ResultSet results;
+    private ResultSet mobo;
+    private ResultSet cpu;
+    private ResultSet ram;
+    private ResultSet gpu;
+    private ResultSet storage;
+
+    public ResultSet getMobo() {
+        return mobo;
+    }
+
+    public ResultSet getCpu() {
+        return cpu;
+    }
+
+    public ResultSet getRam() {
+        return ram;
+    }
+
+    public ResultSet getGpu() {
+        return gpu;
+    }
+
+    public ResultSet getStorage() {
+        return storage;
+    }
+
     private JFrame frame = new JFrame();
     private DBconnection dBconnection;
+    private ArrayList<String> listOfMobos;
+    private ArrayList<String> listOfCPUs;
+    private ArrayList<String> listOfRAMs;
+    private ArrayList<String> listOfGPUs;
+    private ArrayList<String> listOfStorage;
+
+    public ArrayList<String> getListOfStorage() {
+        return listOfStorage;
+    }
+
+    public ArrayList<String> getListOfMobos() {
+        return listOfMobos;
+    }
+
+    public ArrayList<String> getListOfCPUs() {
+        return listOfCPUs;
+    }
+
+    public ArrayList<String> getListOfRAMs() {
+        return listOfRAMs;
+    }
+
+    public ArrayList<String> getListOfGPUs() {
+        return listOfGPUs;
+    }
 
     public GetAllComponentsCommand(DBconnection dBconnection) {
         this.dBconnection = dBconnection;
+        listOfMobos = new ArrayList<String>();
+        listOfCPUs = new ArrayList<String>();
+        listOfRAMs = new ArrayList<String>();
+        listOfGPUs = new ArrayList<String>();
+        listOfStorage = new ArrayList<String>();
     }
 
-    public ResultSet getResults() {
-        return results;
-    }
-
-    public void setResults(ResultSet results) {
-        this.results = results;
-    }
-
-    private boolean IsBuilderWantsPC(Class o){
+    public boolean IsBuilderWantsPC(Class o){
         return o.isInstance(newPC);
     }
     @Override
     public void execute() {
         try{
-            if(!IsBuilderWantsPC(Objects.class)) {
+            /*if(!IsBuilderWantsPC(Objects.class)) {
                 this.setResults(this.dBconnection.getDbConnection().createStatement().executeQuery("select * from alaplap, cpu, ram,gpu,hattertar,monitor,billlentyuzet,touchpad"));
                 logger.info("Sikeres laptop alkatrészek lekérdezés!");
-            }
-            this.setResults(this.dBconnection.getDbConnection().createStatement().executeQuery("select * from alaplap, cpu, ram,gpu,hattertar,tap,gephaz"));
-            logger.info("Sikeres PC alkatrészek lekérdezés!");
+            }*/
+
+                this.mobo = this.dBconnection.getDbConnection().createStatement().executeQuery("select id,gyarto,foglalat,ram_tipus from alaplap");
+                while (this.mobo.next()){
+                    listOfMobos.add(this.mobo.getString("id"));
+                    listOfMobos.add(this.mobo.getString("gyarto"));
+                    listOfMobos.add(this.mobo.getString("foglalat"));
+                    listOfMobos.add(this.mobo.getString("ram_tipus"));
+                }
+                this.cpu = this.dBconnection.getDbConnection().createStatement().executeQuery("select  id,gyarto,tipus,foglalat from cpu");
+                while (this.cpu.next()){
+                    listOfCPUs.add(this.cpu.getString("id"));
+                    listOfCPUs.add(this.cpu.getString("gyarto"));
+                    listOfCPUs.add(this.cpu.getString("tipus"));
+                    listOfCPUs.add(this.cpu.getString("foglalat"));
+                }
+                this.ram = this.dBconnection.getDbConnection().createStatement().executeQuery("select  id,gyarto,ram_tipus from ram");
+                while (this.ram.next()){
+                    listOfRAMs.add(this.ram.getString("id"));
+                    listOfRAMs.add(this.ram.getString("gyarto"));
+                    listOfRAMs.add(this.ram.getString("ram_tipus"));
+                }
+                this.gpu = this.dBconnection.getDbConnection().createStatement().executeQuery("select id,gyarto,vram from gpu");
+                while(this.gpu.next()){
+                    listOfGPUs.add(this.gpu.getString("id"));
+                    listOfGPUs.add(this.gpu.getString("gyarto"));
+                    listOfGPUs.add(this.gpu.getString("vram"));
+                }
+                this.storage = this.dBconnection.getDbConnection().createStatement().executeQuery("select id,gyarto,meret,ssd_e from hattertar");
+                while (this.storage.next()){
+                    listOfStorage.add(this.storage.getString("id"));
+                    listOfStorage.add(this.storage.getString("gyarto"));
+                    listOfStorage.add(this.storage.getString("meret"));
+                    listOfStorage.add(this.storage.getString("ssd_e"));
+                }
+                logger.info("Sikeres PC alkatrészek lekérdezés!");
+                logger.info(listOfMobos);
+                logger.info(listOfCPUs);
         }
         catch (Exception e){
             logger.error(e.getMessage());
